d199d70155c86800e56cb800ba8803f0aa7c273f
Aleksandr Poliakov <alpoliakov73@gmail.com>
2017-04-08 18:43:05 +0300

    Start xCode

---
+++ StartC/StartC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StartC
+//
+//  Created by Aleksandr Poliakov on 08.04.17.
+//  Copyright © 2017 Aleksandr Poliakov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
